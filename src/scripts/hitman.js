6ccde0c00936916edfcd03e4ea16a86c096531a5
Manuela Bechara Cannizza <141856664+binbaragi@users.noreply.github.com>
2025-02-25 20:29:57 -0300

    correção
    apos contratar um assassino o usuario é direcionado para a home page

--- src/scripts/hitman.js
+++ src/scripts/hitman.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     buttons.forEach(button => {
         button.addEventListener("click", function () {
             alert("Contrato enviado para o seu e-mail! Verifique sua caixa de entrada.");
+            window.location.href = "../../index.html";
         });
     });
 });
