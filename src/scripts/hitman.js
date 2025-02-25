5deecb8ee58eba87df58ce5298a600efd25ed5bb
Manuela Bechara Cannizza <141856664+binbaragi@users.noreply.github.com>
2025-02-25 20:15:25 -0300

    {feat} contratacao de assassinos
    criacao do html css e js pra funcao de contratar assassinos

---
+++ src/scripts/hitman.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Seleciona todos os botões de contratar
+    const buttons = document.querySelectorAll(".submit-button");
+
+    // Adiciona o evento de clique a cada botão
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            alert("Contrato enviado para o seu e-mail! Verifique sua caixa de entrada.");
+        });
+    });
+});
